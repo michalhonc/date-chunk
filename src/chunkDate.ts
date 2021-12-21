0761cdb59fe8ff36186b75da5e718d7ddea29fce
Michal Honc <honc355@gmail.com>
2021-12-21 12:12:14 +0100

    Abstract both functions

--- src/chunkDate.ts
+++ src/chunkDate.ts
@@ -1,4 +1,4 @@
-import type { ChunkDateOptions, Dates } from './types'
+import type { ChunkDateOptions, Dates, MaxLimitPerChunkOptions, ChunksOptions } from './types'
 import { isDate, isNumber } from './utils/typeCheck'
 import { getTimestamp, timeTypeList } from './utils/common'
 
@@ -42,60 +42,67 @@ export function chunkDate(options: ChunkDateOptions): Dates {
     return []
   }
 
-  const totalTimestampDiff = getTimestamp(end) - getTimestamp(start)
-
   if ('chunks' in options) {
-    const dates: Dates = []
-
-    let chunkTimestamp = totalTimestampDiff / options.chunks
-    if (options.strictSizedChunks) {
-      chunkTimestamp = Number(chunkTimestamp.toFixed())
-    }
-
-    let prevStartDate = getTimestamp(start)
-
-    for (let i = 0; i < options.chunks; i++) {
-      const endDate = prevStartDate + chunkTimestamp
-
-      dates.push({
-        start: isDate(start) ? new Date(prevStartDate) : prevStartDate,
-        end: isDate(end) ? new Date(endDate) : endDate,
-      })
-
-      prevStartDate = endDate
-    }
-
-    return dates
+    return chunkEven(options)
   }
 
-  if (
-    Array.isArray(options.maxLimitPerChunk) &&
-    typeof options.maxLimitPerChunk[0] === 'number' &&
-    typeof timeTypeList[options.maxLimitPerChunk[1]] === 'number'
-  ) {
-    const dates: Dates = []
-    const timeRange = options.maxLimitPerChunk[0] * timeTypeList[options.maxLimitPerChunk[1]]
-
-    let chunkRange = totalTimestampDiff / timeRange
-
-    let prevStartDate = getTimestamp(start)
-
-    while (chunkRange > 0) {
-      const denominator = chunkRange >= 1 ? 1 : chunkRange
-      const endDate = prevStartDate + timeRange * denominator
-
-      dates.push({
-        start: isDate(start) ? new Date(prevStartDate) : prevStartDate,
-        end: isDate(end) ? new Date(endDate) : endDate,
-      })
-
-      prevStartDate = endDate
-
-      chunkRange--
-    }
-
-    return dates
+  if ('maxLimitPerChunk' in options) {
+    return chunkBySize(options)
   }
 
   return []
+}
+
+function chunkEven(options: ChunksOptions): Dates {
+  const dates: Dates = []
+  const totalTimestampDiff = getTimestamp(options.end) - getTimestamp(options.start)
+
+  let chunkTimestamp = totalTimestampDiff / options.chunks
+  if (options.strictSizedChunks) {
+    chunkTimestamp = Number(chunkTimestamp.toFixed())
+  }
+
+  let prevStartDate = getTimestamp(options.start)
+
+  for (let i = 0; i < options.chunks; i++) {
+    const endDate = prevStartDate + chunkTimestamp
+
+    dates.push({
+      start: isDate(options.start) ? new Date(prevStartDate) : prevStartDate,
+      end: isDate(options.end) ? new Date(endDate) : endDate,
+    })
+
+    prevStartDate = endDate
+  }
+
+  return dates
+}
+
+function chunkBySize(options: MaxLimitPerChunkOptions): Dates {
+  const timeValue = options.maxLimitPerChunk[0];
+  const timeDenominator = timeTypeList[options.maxLimitPerChunk[1]];
+
+  const dates: Dates = []
+  const totalTimestampDiff = getTimestamp(options.end) - getTimestamp(options.start)
+  const timeRange = timeValue * timeDenominator
+
+  let chunkRange = totalTimestampDiff / timeRange
+
+  let prevStartDate = getTimestamp(options.start)
+
+  while (chunkRange > 0) {
+    const denominator = chunkRange >= 1 ? 1 : chunkRange
+    const endDate = prevStartDate + timeRange * denominator
+
+    dates.push({
+      start: isDate(options.start) ? new Date(prevStartDate) : prevStartDate,
+      end: isDate(options.end) ? new Date(endDate) : endDate,
+    })
+
+    prevStartDate = endDate
+
+    chunkRange--
+  }
+
+  return dates
 }
